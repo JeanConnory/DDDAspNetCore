32b45c16afe18f4a4c1ec67e7cdef0e947d2c507
Jean Connory <michaelrhcp@gmail.com>
2023-05-26 10:58:51 -0300

    BaseTest

---
+++ src/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,48 @@
+using System;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+
+    public class DbTeste : IDisposable
+    {
+        private string dataBaseName = $"dbDddApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public BaseTest()
+        {
+            var connectionStringTest = $"Persist Security Info=True;Server=Localhost;Port=3306;Database={dataBaseName};Uid=root;Pwd=123456789";
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseMySql(connectionStringTest, ServerVersion.AutoDetect(connectionStringTest)),
+                ServiceLifetime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
